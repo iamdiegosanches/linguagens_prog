a221e3c55ba51ad18419c0fc7605cacd63064663
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2024-12-15 11:33:15 -0300

    Update ex7_verifica_pares.pl

--- exemplos_extra/ex7_verifica_pares.pl
+++ exemplos_extra/ex7_verifica_pares.pl
@@ -1,7 +1,11 @@
-pares(L, P) :- verifica_pares(L, [], P).
+% pares(L, P) :- verifica_pares(L, [], P).
 
-verifica_pares([], P, P).
-verifica_pares([H|T], Aux, P) :- par(H), !, verifica_pares(T, [H|Aux] , P).
-verifica_pares([_|T], Aux, P) :- verifica_pares(T, Aux, P).
+% verifica_pares([], P, P).
+% verifica_pares([H|T], Aux, P) :- par(H), !, verifica_pares(T, [H|Aux] , P).
+% verifica_pares([_|T], Aux, P) :- verifica_pares(T, Aux, P).
 
-par(N):- N mod 2 =:= 0.
+% par(N):- N mod 2 =:= 0.
+
+pares([],[]).
+pares([K|T], [K|P]) :- K mod 2 =:= 0, pares(T, P).
+pares([K|T], P) :- K mod 2 =\= 0, pares(T, P).
