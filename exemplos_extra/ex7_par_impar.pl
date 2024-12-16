3fd9908a6aae1389d7bb5dfc2c36fccfbc32cd99
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2024-12-16 18:59:58 -0300

    Update and rename ex7_verifica_pares.pl to ex7_par_impar.pl

--- exemplos_extra/ex7_par_impar.pl
+++ exemplos_extra/ex7_par_impar.pl
@@ -9,3 +9,7 @@
 pares([],[]).
 pares([K|T], [K|P]) :- K mod 2 =:= 0, pares(T, P).
 pares([K|T], P) :- K mod 2 =\= 0, pares(T, P).
+
+impares([],[]).
+impares([K|T], [K|P]) :- K mod 2 =\= 0, impares(T, P).
+impares([_|T], P) :-  impares(T, P).
