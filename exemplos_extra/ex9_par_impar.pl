ed17e9662620422b803ad932c4fa2207a0f0c711
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2024-12-16 19:00:08 -0300

    Delete exemplos_extra/ex9_par_impar.pl

--- exemplos_extra/ex9_par_impar.pl
+++
@@ -1,7 +0,0 @@
-pares([],[]).
-pares([K|T], [K|P]) :- K mod 2 =:= 0, pares(T, P).
-pares([K|T], P) :- K mod 2 =\= 0, pares(T, P).
-
-impares([],[]).
-impares([K|T], [K|P]) :- K mod 2 =\= 0, impares(T, P).
-impares([_|T], P) :-  impares(T, P).
